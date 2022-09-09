b8e0c8b982331a7fff9cfff7b7ea375c976808bb
Noe <VaescoNoe@gmail.com>
2022-09-09 12:25:32 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Persona p = new Persona();
+		p.setNombre("Noe");
+		p.setEdad(30);
+		p.setTelefono("1212345678");
+		
+		System.out.println(p.toString());
+	}
+}
